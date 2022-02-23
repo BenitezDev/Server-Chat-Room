1fa0a3ceadc06ae4a3483cee3abab26ca5525a60
BenitezDev <benitezdev@gmail.com>
2022-02-23 21:03:04 +0100

    add server args

--- Server/Server/Server.py
+++ Server/Server/Server.py
@@ -1,15 +1,16 @@
 import socket
 import threading
+import argparse
 
 class Server:
        
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     connections = []
 
-    def __init__(self):
-       
-        self.sock.bind(('127.0.0.1', 8080))
-        self.sock.listen(10)
+    def __init__(self, address, port, connections):
+        self.sock.bind((address, port))
+        self.sock.listen(connections)
+        print(address, port, connections)
 
     def handler(self, conn, addr):
         while True:
@@ -30,5 +31,12 @@ class Server:
             print(self.connections)
 
 
-server = Server()
+parser = argparse.ArgumentParser()
+parser.add_argument('--address',    type=str, required=False, help='The address of the server'        ,  default='127.0.0.1')
+parser.add_argument('--port',       type=int, required=False, help='The port of the server'           ,  default=8080       )
+parser.add_argument('--conections', type=int, required=False, help='The maximum number of connections',  default=10         )
+args = parser.parse_args()
+
+server = Server(args.address, args.port, args.conections)
 server.run()
+
