13b9a95a489c4aebcca5b6ade18b7da0673a1e1a
BenitezDev <benitezdev@gmail.com>
2022-02-23 20:46:03 +0100

    add very basic behaviour

--- Client/Client/Program.cs
+++ Client/Client/Program.cs
@@ -1,1 +1,35 @@
-﻿Console.WriteLine("Im the client");
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        IPAddress ip = IPAddress.Parse("127.0.0.1");
+        int port = 8080;
+        TcpClient client = new TcpClient();
+        client.Connect(ip, port);
+        Console.WriteLine("client connected!!");
+        NetworkStream ns = client.GetStream();
+
+        string s;
+        while (true)
+        {
+            s = Console.ReadLine();
+            byte[] buffer = Encoding.ASCII.GetBytes(s);
+            ns.Write(buffer, 0, buffer.Length);
+            byte[] receivedBytes = new byte[1024];
+            int byte_count = ns.Read(receivedBytes, 0, receivedBytes.Length);
+            byte[] formated = new byte[byte_count];
+            //handle  the null characteres in the byte array
+            Array.Copy(receivedBytes, formated, byte_count);
+            string data = Encoding.ASCII.GetString(formated);
+            Console.WriteLine(data);
+        }
+        ns.Close();
+        client.Close();
+        Console.WriteLine("disconnect from server!!");
+        Console.ReadKey();
+    }
+}
--- Server/Server/Server.py
+++ Server/Server/Server.py
@@ -1,1 +1,34 @@
-print("Im the server")
+import socket
+import threading
+
+class Server:
+       
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    connections = []
+
+    def __init__(self):
+       
+        self.sock.bind(('127.0.0.1', 8080))
+        self.sock.listen(10)
+
+    def handler(self, conn, addr):
+        while True:
+            data = conn.recv(1024)
+            for connection in self.connections:
+                connection.send(data)
+                print(data)
+            if not data:
+                break
+
+    def run(self):
+        while True:
+            conn ,addr = self.sock.accept()
+            cThread = threading.Thread(target=self.handler, args=(conn, addr))
+            cThread.daemon = True
+            cThread.start()
+            self.connections.append(conn)
+            print(self.connections)
+
+
+server = Server()
+server.run()
