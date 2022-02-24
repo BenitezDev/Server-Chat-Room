c025e5bef94257598def58a1ca155231525749fe
BenitezDev <benitezdev@gmail.com>
2022-02-24 19:08:52 +0100

    for the time being, finished :s

--- Client/Client/Program.cs
+++ Client/Client/Program.cs
@@ -1,35 +1,151 @@
-﻿using System.Net;
-using System.Net.Sockets;
+﻿using System.Net.Sockets;
 using System.Text;
+
+
 
 class Program
 {
     static void Main(string[] args)
     {
-        IPAddress ip = IPAddress.Parse("127.0.0.1");
-        int port = 8080;
-        TcpClient client = new TcpClient();
-        client.Connect(ip, port);
-        Console.WriteLine("client connected!!");
-        NetworkStream ns = client.GetStream();
+        if (args.Length != 3)
+        {
+            Console.WriteLine("Please run the Client with the nickname, address and port as arguments.");
+        }
+        else
+        {
+            Client myClient = new Client(args[0], args[1], int.Parse(args[2]));
+        }
+    }
+}
 
-        string s;
+public class Client
+{
+    private string    nickname;
+    private TcpClient clientSocket;
+    
+    private Thread    receiveThread;
+    private Thread    sendThread;
+    private Thread    logInThread;
+
+    private NetworkStream serverStream;
+    int bufferSize = 1024;
+
+    public Client(string name, string ip, int port)
+    {
+        clientSocket = new TcpClient();
+        clientSocket.Connect(ip, port);
+        serverStream = clientSocket.GetStream();
+
+        nickname = name;
+        
+        LogIn();
+    }
+ 
+    /// <summary>
+    /// Log into the server with the given nickname
+    /// </summary>
+    void LogIn()
+    {
+        // Send the Client nickname to the server
+        SendMsg(nickname);
+
+        // Get the server response. 
+        // Server response are filled with \0 to fill the buffer
+        string returndata = ReadSocket().Replace("\0", string.Empty);
+        Console.WriteLine("@server: " + returndata);
+
+        if (returndata != "Connected to server!")
+        {
+            ExitChat();
+        }
+        else
+        {
+            try
+            {
+                // Start threading
+                receiveThread = new Thread(new ThreadStart(getMessage));
+                receiveThread.Start();
+
+                sendThread = new Thread(new ThreadStart(Chat));
+                sendThread.Start();
+            }
+            catch (Exception e)
+            {
+                Console.Out.WriteLine("Error on threading " + e);
+            }
+
+        }
+    }
+
+    /// <summary>
+    /// Get message from the server and print it
+    /// </summary>
+    private void getMessage()
+    {
         while (true)
         {
-            s = Console.ReadLine();
-            byte[] buffer = Encoding.ASCII.GetBytes(s);
-            ns.Write(buffer, 0, buffer.Length);
-            byte[] receivedBytes = new byte[1024];
-            int byte_count = ns.Read(receivedBytes, 0, receivedBytes.Length);
-            byte[] formated = new byte[byte_count];
-            //handle  the null characteres in the byte array
-            Array.Copy(receivedBytes, formated, byte_count);
-            string data = Encoding.ASCII.GetString(formated);
-            Console.WriteLine(data);
+            Console.WriteLine(ReadSocket());
         }
-        ns.Close();
-        client.Close();
-        Console.WriteLine("disconnect from server!!");
-        Console.ReadKey();
+    }
+
+    /// <summary>
+    /// Allow the client to write and send message to the server
+    /// </summary>
+    void Chat()
+    {
+        string? menssage = null;
+        while (menssage != "/exit")
+        {
+            while (menssage == null)
+            {
+                menssage = Console.ReadLine();
+            }
+            SendMsg(menssage);
+            menssage = null;
+        }
+        ExitChat();
+    }
+
+    /// <summary>
+    /// Send a message to the server
+    /// </summary>
+    /// <param name="msg">message to send</param>
+    void SendMsg(string msg)
+    {
+        if (clientSocket == null) return;
+
+        try
+        {
+            byte[] buffer = new byte[bufferSize];
+            buffer = Encoding.ASCII.GetBytes(msg);
+            serverStream.Write(buffer, 0, buffer.Length);
+        }
+        catch (SocketException socketException)
+        {
+            Console.Out.WriteLine("Socket exception: " + socketException);
+        }
+    }
+
+    /// <summary>
+    /// Read from socket
+    /// </summary>
+    /// <returns>socket data converted into ASCII string</returns>
+    private string ReadSocket()
+    {
+        byte[] buffer = new byte[bufferSize];
+        serverStream.Read(buffer, 0, buffer.Length);
+        string response = Encoding.ASCII.GetString(buffer);
+        return response;
+    }
+
+    /// <summary>
+    /// Close threads and connection to the server
+    /// </summary>
+    void ExitChat()
+    {
+        receiveThread.Abort();
+        logInThread.Abort();
+        serverStream.Close();
+        clientSocket.Close();
     }
 }
--- Server/Server/Server.py
+++ Server/Server/Server.py
@@ -1,42 +1,79 @@
 import socket
 import threading
 import argparse
+import time
+
+buffer_size = 1024
 
 class Server:
        
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    connections = []
+    clients = []
+    nicknames = []
 
     def __init__(self, address, port, connections):
         self.sock.bind((address, port))
         self.sock.listen(connections)
-        print(address, port, connections)
+        print(f"Server listening in {address}:{port}")
 
-    def handler(self, conn, addr):
-        while True:
-            data = conn.recv(1024)
-            for connection in self.connections:
-                connection.send(data)
-                print(data)
-            if not data:
-                break
+    # Sending Messages To All Connected Clients
+    def broadcast(self, message):
+        for client in self.clients:
+            client.send(message.decode('ascii'))
 
-    def run(self):
+    # Handling Messages From Clients
+    def handle(self, client):
         while True:
-            conn ,addr = self.sock.accept()
-            cThread = threading.Thread(target=self.handler, args=(conn, addr))
-            cThread.daemon = True
-            cThread.start()
-            self.connections.append(conn)
-            print(self.connections)
+            try:
+                # Broadcasting Messages
+                # here is error :(
+                message = client.recv(buffer_size).decode('ascii')
+                print(message)
+                self.broadcast(message)
+
+            except:
+                # Removing And Closing Clients
+                index = self.clients.index(client)
+                # self.broadcast('{} left!'.format(nickname).encode('ascii'))
+                self.clients.remove(client)
+                client.close()
+                nickname = self.nicknames[index]
+                self.nicknames.remove(nickname)
+
+
+    # Receiving / Listening Function
+    def receive(self):
+        while True:
+            # Accept new connections
+            client, address = self.sock.accept()
+            print("Connected with {}".format(str(address)))
+
+            # Store Nickname            
+            nickname = client.recv(buffer_size).decode('ascii')
+
+            if nickname not in self.nicknames:
+                print(nickname, "enter the chat!")
+                self.nicknames.append(nickname)
+                self.clients.append(client)
+                client.send('Connected to server!'.encode('ascii'))
+            else:
+                client.send('Nickname already in use.'.encode('ascii'))
+                continue
+
+            # Print And Broadcast Nickname
+            # self.broadcast("{} joined!".format(nickname).encode('ascii'))
+
+            # Start Handling Thread For Client
+            thread = threading.Thread(target=self.handle, args=(client,))
+            thread.start()
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--address',    type=str, required=False, help='The address of the server'        ,  default='127.0.0.1')
+parser.add_argument('--address',    type=str, required=False, help='The IP address of the server'     ,  default='127.0.0.1')
 parser.add_argument('--port',       type=int, required=False, help='The port of the server'           ,  default=8080       )
 parser.add_argument('--conections', type=int, required=False, help='The maximum number of connections',  default=10         )
 args = parser.parse_args()
 
 server = Server(args.address, args.port, args.conections)
-server.run()
+server.receive()
 
